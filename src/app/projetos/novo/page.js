4152813e4a9f2d9d5847bc7628bff3ba96257303
Lucas Pinheiro <lucaspinheiro863@gmail.com>
2026-08-16 10:43:16 -0400

    fix: implementa pagina de novo projeto

---
+++ src/app/projetos/novo/page.js
@@ -0,0 +1,12 @@
+import Navegacao from "@/components/Navegacao";
+
+export default function NovoProjetoPage() {
+  return (
+    <main>
+      <Navegacao />
+
+      <h1>Novo projeto</h1>
+      <p>Aqui criaremos o formulário de cadastro.</p>
+    </main>
+  );
+}
