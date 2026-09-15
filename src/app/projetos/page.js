91e99c8e8867bf22463d0c222331dfd7518a0a61
Lucas Pinheiro <lucaspinheiro863@gmail.com>
2026-09-15 11:04:58 -0400

    feat: renderiza lista de projetos com map

--- src/app/projetos/page.js
+++ src/app/projetos/page.js
@@ -1,22 +1,41 @@
 import CardProjeto from "@/components/CardProjeto";
 
+const projetos = [
+  {
+    id: 1,
+    nome: "SmartPlateAI",
+    descricao:
+      "Aplicação de planejamento alimentar com inteligência artificial.",
+    tecnologia: "Next.js + OpenAI",
+  },
+  {
+    id: 2,
+    nome: "Web Full-Stack Lab",
+    descricao: "Projeto utilizado para estudar desenvolvimento full-stack.",
+    tecnologia: "Next.js",
+  },
+  {
+    id: 3,
+    nome: "Central de Indicadores",
+    descricao: "Painel para acompanhamento de Indicadores Corporativos ",
+    tecnologia: "Nuxt + FastAPI",
+  },
+];
+
 export default function ProjetosPage() {
   return (
     <main>
       <h1>Meus Projetos</h1>
       <p>Aqui ficarão os projetos cadastrados.</p>
 
-      <CardProjeto
-        nome="Web Full-Stack Lab"
-        descricao="Projeto utilizado para estudar desenvolvimento full-stack."
-        tecnologia="Next.js"
-      />
-
-      <CardProjeto
-        nome="SmartPlate"
-        descricao="Aplicação de planejamento alimentar com inteligência artificial."
-        tecnologia="Next.js + OpenAI"
-      />
+      {projetos.map((projeto) => (
+        <CardProjeto
+          key={projeto.id}
+          nome={projeto.nome}
+          descricao={projeto.descricao}
+          tecnologia={projeto.tecnologia}
+        />
+      ))}
     </main>
   );
 }
